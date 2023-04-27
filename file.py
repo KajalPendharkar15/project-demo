b45c06a4fa9106b4f1967359e5795d8c150d2868
kajal pendharkar <kajalpend15@gmail.com>
2023-04-27 13:53:57 +0530

    test code

---
+++ file.py
@@ -0,0 +1,17 @@
+f= open(r"C:\Users\Kajal\OneDrive\Desktop\project\hello.txt", 'r')
+if f:
+    print("File is successfully opened")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+y = np.array([35, 25, 25, 15])
+
+plt.pie(y)
+plt.show() 
